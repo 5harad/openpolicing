77af3b80d9f936f5387cc9a1f6c7713aef106d84
Yulan Lin <yulan.lin.2014@gmail.com>
2017-06-24 10:11:50 -0500

    edits

--- stardust/js/main.js
+++
@@ -1,8 +0,0 @@
-// Get our canvas element
-var canvas = document.getElementById("main-canvas");
-var width = 960;
-var height = 500;
-
-// Create a WebGL 2D platform on the canvas:
-var platform = Stardust.platform("webgl-2d", canvas, width, height);
-// ... Load data and render your visualization
